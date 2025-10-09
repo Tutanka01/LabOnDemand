76d1b6a06ec00c64ad3f46841d05ce97503c3f8e
Mohamad El Akhal <meaebouzidi@gmail.com>
2025-10-09 09:35:08 +0200

    netbeans fonctionnel

--- dockerfiles/base-images/VNC/src/common/startup_scripts/startup.sh
+++ dockerfiles/base-images/VNC/src/common/startup_scripts/startup.sh
@@ -10,21 +10,9 @@ Xvfb :${DISPLAY_NUM} -screen 0 ${GEOMETRY} &
 export DISPLAY=:${DISPLAY_NUM}
 sleep 1
 
-# Démarre le bureau XFCE en arrière-plan
+# Démarre le bureau XFCE (qui lancera notre script de fond d'écran via l'autostart)
 startxfce4 &
 sleep 2
-
-# --- DÉBUT DE LA MODIFICATION FINALE ---
-# On force le fond d'écran de manière robuste.
-WALLPAPER_PATH="/usr/share/backgrounds/bg_default.png"
-
-# Trouve TOUTES les propriétés de fond d'écran et boucle dessus
-xfconf-query -c xfce4-desktop -l | grep 'last-image$' | while read -r PROPERTY; do
-  # Applique notre fond d'écran à chaque propriété trouvée (monitor0, monitorscreen, etc.)
-  echo "Définition du fond d'écran pour la propriété : $PROPERTY"
-  xfconf-query -c xfce4-desktop -p "$PROPERTY" -s "$WALLPAPER_PATH"
-done
-# --- FIN DE LA MODIFICATION FINALE ---
 
 # Démarre le serveur VNC x11vnc
 x11vnc -forever -passwd "$VNC_PW" -display :${DISPLAY_NUM} &
